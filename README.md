e3efe209f1a4d3b8463379f2a4932b0fb080ed90
KeisukeKudo <melchior.balthasar.casper326@gmail.com>
2017-11-15 14:45:41 +0900

    Complete to chapter8.

--- README.md
+++ README.md
@@ -3,4 +3,7 @@
 [1日で理解するDjango超基礎入門](https://www.amazon.co.jp/1%E6%97%A5%E3%81%A7%E7%90%86%E8%A7%A3%E3%81%99%E3%82%8BDjango%E8%B6%85%E5%9F%BA%E7%A4%8E%E5%85%A5%E9%96%80-%E6%B7%B5%E4%B8%8A-%E5%96%9C%E5%BC%98-ebook/dp/B071S25M33)
 
 写経  
-[チャプタ7まで](https://github.com/KeisukeKudo/PythonTestProject)
+[チャプタ7まで](https://github.com/KeisukeKudo/PythonTestProject)  
+2017/11/15 チャプタ8まで了  
+※削除機能の説明が不足していて未実装  
+時間が勿体無いので次へ
--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import MessageBord
 
-# Create your views here.
+
+def MyAppIndex(request):
+    msg = request.GET.get('words')
+
+    delete_text = request.POST.getlist('delete_text')
+    if delete_text:
+        MessageBord.objects.filter(id__in=delete_text).delete()
+
+    data_list = MessageBord.objects.order_by('-id')
+    contexts = {
+        'result_list': data_list
+    }
+
+    if msg is not None:
+        message_data = MessageBord()
+        message_data.new_message = msg
+        message_data.save()
+
+    return render(request, 'myapp/index.html', contexts)
